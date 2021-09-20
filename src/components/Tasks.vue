622889ec6fa254ffbfc45236059479c2b5277463
evazquez94 <eduardo940531@gmail.com>
2021-09-20 18:55:54 +0200

    Add design and static tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3 :key="task.id" v-for="task in tasks">
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </h3>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
